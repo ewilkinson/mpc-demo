Adding seperate simulation state. Fixing bugs in minimization calculation, although one big one remains. Intend on making display only render previously simulated data.

bodies.py
@@ -71,8 +71,10 @@ class AbstractBody(object):
 
     def __init__(self, window):
         self.window = window
-        self.window_x = window.size[0]
-        self.window_y = window.size[1]
+
+        if window is not None:
+            self.window_x = window.size[0]
+            self.window_y = window.size[1]
 
         self.EULER = AbstractBody.EULER
         self.RUNGE_KUTTA = AbstractBody.RUNGE_KUTTA
@@ -113,6 +115,10 @@ class AbstractBody(object):
         """
         return Param.list_to_matrix(self.state)
 
+    def set_state(self, X):
+        for i in range(X.shape[0]):
+            self.state[i].value = X[i, 0]
+
     def integrate(self, X, dt, u=0):
         # Euler
         if self.integration_type == self.EULER:
@@ -131,6 +137,11 @@ class AbstractBody(object):
 
         return X + X_prime * dt, X_prime
 
+    def add_max_value_constraint(self, indx, value):
+        self.cons += ({'type': 'ineq',
+                      'fun': lambda x: np.array([-abs(x[indx]) + value]),  # max pendulum torque
+                      'jac': lambda x: np.array([0.0, -1.0]) if x[1] > 0 else np.array([0.0, 1.0])},)
+
 
 class SpringDamper(AbstractBody):
     def __init__(self, window, pos_x=0.0, pos_y=0.0):
@@ -144,11 +155,11 @@ class SpringDamper(AbstractBody):
         self.state = [self.pos_x, self.pos_x_d]
 
         self.poly_radius = 10
-        self.renderables = self._create_renderables()
 
-        self.cons = ({'type': 'ineq',
-                      'fun': lambda x: np.array([-abs(x[1]) + 3]),  # max pendulum torque
-                      'jac': lambda x: np.array([0.0, -1.0]) if x[1] > 0 else np.array([0.0, 1.0])})
+        if window is not None:
+            self.renderables = self._create_renderables()
+
+        self.add_max_value_constraint(1, 20)
 
     def set_properties(self, k, m, c, x0):
         self.k = k
@@ -157,7 +168,10 @@ class SpringDamper(AbstractBody):
         self.x0 = x0
 
         self.A = np.matrix([[0, 1], [-self.k / self.m * (1 - self.x0 / (self.pos_x + 1e-5)), -self.c / self.m]])
-        self.B = np.matrix([0, 1 / self.m])
+
+        self.B = np.matrix(np.zeros(shape=(2, 2)))
+        self.B[1, 1] =1 / self.m
+
         self.u = np.matrix(np.zeros(shape=(self.B.shape[1], 1)))
 
     def _create_renderables(self):
@@ -168,13 +182,7 @@ class SpringDamper(AbstractBody):
         return [self.polygon]
 
     def set_position(self, pos_x, pos_y):
-        self.pos_x.value = pos_x * 1.
-        self.pos_y.value = pos_y * 1.
         super(SpringDamper, self).set_position(pos_x, pos_y)
-
-    def set_velocity(self, pos_x_d, pos_y_d):
-        self.pos_x_d.value = pos_x_d
-        self.pos_y_d.value = pos_y_d
 
     def simulate(self, dt, u=0):
         x1 = self.state[0].value
@@ -183,7 +191,13 @@ class SpringDamper(AbstractBody):
         X, X_prime = self.integrate(self.get_state(), dt, u)
 
         self.set_position(X[0,0], self.pos_y.value)
-        self.set_velocity(X[1,0], self.pos_x_d.value)
+        self.set_state(X)
+
+    def set_state(self, X):
+        super(SpringDamper, self).set_state(X)
+
+        x1 = self.state[0].value
+        self.A[1, 0] = -self.k / self.m * (1 - self.x0 / (x1 + 1e-7))
 
 
 class Pendulum(AbstractBody):
@@ -220,7 +234,8 @@ class Pendulum(AbstractBody):
 
         self.u = np.matrix(np.zeros(shape=(self.B.shape[1], 1)))
 
-        self.renderables = self._create_renderables()
+        if self.window is not None:
+            self.renderables = self._create_renderables()
 
     def _create_renderables(self):
         size_x = int(self.L * self.window_x)
@@ -313,7 +328,8 @@ class CartPole(AbstractBody):
 
         self.u = np.matrix(np.zeros(shape=(self.B.shape[1], 1)))
 
-        self.renderables = self._create_renderables()
+        if self.window is not None:
+            self.renderables = self._create_renderables()
 
     def _create_renderables(self):
         size_x = int(self.l * self.window_x)

display.py
@@ -24,6 +24,7 @@ def add_time(window, time):
 
 class System(object):
     def __init__(self, window, dt=0.01):
+        self.use_mpc = True
         self.clock = sf.Clock()
         self.window = window
         self.dt = dt
@@ -53,13 +54,15 @@ class System(object):
         self.mpc = MPC(dt, self.mpc_horizon)
 
         # This works for springdamper and pendulum
-        R = np.matrix(np.eye(2) * 0.0)
-        Q = np.matrix(np.matrix([[100, 0], [0.0, 0.0]]))
-        self.mpc.set_cost_weights(Q, R)
+        R = np.matrix(np.eye(2) * 1e-5)
+        Q = np.matrix(np.matrix([[100, 0], [0.0, 1.0]]))
+        T = np.matrix(np.eye(2) * 1e-5)
+        self.mpc.set_cost_weights(Q, R, T)
 
         self.mpc.Y_prime = np.matrix(np.zeros(shape=(2, self.mpc_horizon)))
-        self.mpc.Y_prime[0, :] = np.pi  # 0.4
-        self.C = np.matrix(np.eye(2))
+        self.mpc.Y_prime[0, :] = 0.7
+        self.mpc.C = np.matrix(np.eye(2))
+
 
         # cartpole
         # R = np.matrix(np.eye(4)*0.001)
@@ -78,21 +81,24 @@ class System(object):
 
 
         # self.bodies = [spring_damper, pendulum]
-        self.bodies = [pendulum]
+        self.bodies = [spring_damper]
 
         self.mpc.set_body(self.bodies[0])
-        self.mpc.C = self.C  # for now
 
     def simulate(self):
         self.counter += 1
 
-        for body in self.bodies:
+        if self.use_mpc:
             if self.counter % self.mpc_horizon == 0:
                 self.counter = 0
                 self.mpc.calc_control()
 
             u = self.mpc.U[:, self.counter]
-            body.simulate(self.dt, np.matrix(u).transpose())
+            self.mpc.body.simulate(self.dt, np.matrix(u).transpose())
+
+        else:
+            for body in self.bodies:
+                body.simulate(self.dt)
 
     def render(self):
         self.window.clear(sf.Color.WHITE)

mpc.py
@@ -21,15 +21,17 @@ class MPC(object):
         X = np.copy(self.X_iter)
 
         sum_cost = 0
-        for i in range(4):
+        for i in range(2):
             temp_u = (np.matrix(np.zeros(u.shape)), u)[i == 0]  # if / else statement
             X, X_prime = self.body.integrate(X, self.dt, temp_u)
 
             # for debug so I can check each variable separately
             DY = self.y_prime - self.C * X
+            DU = self.u - u
             a = DY.transpose() * self.Q * DY
             b = u.transpose() * self.R * u
-            c = np.sum(a + b)
+            d = DU.transpose() * self.T * DU
+            c = np.sum(a + b + d)
             sum_cost += c
 
         return sum_cost
@@ -47,22 +49,27 @@ class MPC(object):
         self.U = np.zeros(shape=(self.u.shape[0], self.horizon))
         self.Y = np.zeros(shape=(body.A.shape[1], self.horizon))
 
-    def set_cost_weights(self, Q, R):
+    def set_cost_weights(self, Q, R, T=None):
         """
         :type Q: numpy.matrixlib.defmatrix.matrix
         :param Q: Cost matrix for observables
 
         :type R: numpy.matrixlib.defmatrix.matrix
         :param R: Cost matrix for control
+
+        :type T: numpy.matrixlib.defmatrix.matrix
+        :param T: Cost matrix for control
         """
         if type(Q) is not np.matrix or type(R) is not np.matrix:
             raise RuntimeError('Q and R must be numpy matrices')
 
         self.Q = Q
         self.R = R
+        self.T = T
 
     def calc_control(self):
         self.X_iter = np.copy(self.body.get_state())
+        self.U_iter = np.copy(self.U[:, -1])
 
         for i in range(self.horizon):
             self.y_prime = self.Y_prime[:, i]
@@ -73,4 +80,4 @@ class MPC(object):
 
             self.Y[:, i] = np.ravel(self.X_iter)
             self.U[:, i] = np.ravel(u)
-            self.u = np.matrix(np.zeros(self.u.shape))
+            self.u = u